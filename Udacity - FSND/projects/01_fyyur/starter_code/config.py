8887c3c6c06789b2e48d8577c4738f12de619c40
linahhamdan <linah.hamdan@kapsarc.org>
2020-10-16 20:01:59 +0300

    fixed Udacity FSND 01_fyyur project based on review

--- Udacity - FSND/projects/01_fyyur/starter_code/config.py
+++ Udacity - FSND/projects/01_fyyur/starter_code/config.py
@@ -8,8 +8,18 @@ DEBUG = True
 
 # Connect to the database
 
+class DatabaseURI:
+
+    # Just change the names of your database and crendtials and all to connect to your local system
+    DATABASE_NAME = "fyyur"
+    username = 'postgres'
+    password = ''
+    url = '127.0.0.1:5432'
+    SQLALCHEMY_DATABASE_URI = "postgres://{}:{}@{}/{}".format(
+        username, password, url, DATABASE_NAME)
+
 
 # TODO. IMPLEMENT DATABASE URL
 # //username:password@server/db
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@127.0.0.1:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
