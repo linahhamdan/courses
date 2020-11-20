640549d5f0d6aa93f2c7bd6f887d67d0f222cbbb
linahhamdan <linah.alhamdan@gmail.com>
2020-11-20 17:02:23 +0300

    FSND coffee shop project

--- Udacity - FSND/projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Udacity - FSND/projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'dev-8d5ayvrx.eu.auth0.com', // the auth0 domain prefix
-    audience: 'https://dev-8d5ayvrx.eu.auth0.com/api/v2/', // the audience set for the auth0 app
+    audience: 'https://linahhamdan.github.io', // the audience set for the auth0 app
     clientId: '5sc381laMcUPwDVQyt8dthYV0YHUWVKB', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
   }
